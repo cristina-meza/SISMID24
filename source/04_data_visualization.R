edb55d96ce79047898c0db11ab3f559a2733f2b0
Meza <julia.wrobel@emory.edu>
2024-07-23 11:18:36 -0400

    Updated the angle of x axis to 30 and added 2 comments

--- source/04_data_visualization.R
+++ source/04_data_visualization.R
@@ -3,14 +3,14 @@ load(here::here("data", "clean.Rdata"))
 library(tidyverse)
 library(forcats)
 
-
+# add a comment - small change to file added as example
 plot1 = covid %>%
   mutate(county = factor(county),
          county = fct_reorder(county, concentration)) %>%
   ggplot(aes(county, log(concentration), group = county, fill = county)) +
   geom_boxplot() +
   theme(legend.position = "none",
-        axis.text.x = element_text(angle = 45, hjust = 1))
+        axis.text.x = element_text(angle = 30, hjust = 1)) #changed angle to 30 degrees
 
 
 ## gonna do something here with the dates to make this prettier.
